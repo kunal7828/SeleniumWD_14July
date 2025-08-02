7263dafb58e1a0721f4eb8edf9ca9f0895daf5e4
kunalsharma0874 <kunalmqs01@gmail.com>
2025-08-02 11:27:44 +0530

    2 August

--- src/testNGExamples/HRMAdmin_CURD_26.java
+++ src/testNGExamples/HRMAdmin_CURD_26.java
@@ -2,7 +2,6 @@ package testNGExamples;
 
 import org.testng.annotations.Test;
 
-import org.testng.annotations.BeforeClass;
 
 import java.time.Duration;
 
@@ -11,12 +10,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.Reporter;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
